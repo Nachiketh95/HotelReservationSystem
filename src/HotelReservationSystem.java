d3baa0537957eb356df98555326ec3c19dd66fc7
Nachiketh Chiluwari <121386209+Nachiketh95@users.noreply.github.com>
2025-02-26 19:05:04 +0530

    HotelReservationSystem.java

--- src/HotelReservationSystem.java
+++ src/HotelReservationSystem.java
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
